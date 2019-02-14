2feb92312058478427634e80a601be218871f8ed
Trey <tbeau5595@hotmail.com>
2019-02-14 22:52:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -59,6 +59,7 @@ def dennis_ritchies_language
         }
      }
 programmer_hash[:dennis_ritchie][:languages][0]
+programmer_hash
 end
 
 def adding_matz
@@ -84,7 +85,7 @@ def adding_matz
         }
      }
 programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
-    
+programmer_hash   
 end
 
 def changing_alan
@@ -105,8 +106,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
-     
+programmer_hash[:alan_kay][:known_for] = "GUI"
+programmer_hash
 end
 
 def adding_to_dennis
@@ -125,5 +126,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages]<< ""
 end
